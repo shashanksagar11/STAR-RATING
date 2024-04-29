759234c9210ae3da1f65b1236f4f5500e5af2ff7
Shashank <shashanksagar1705@gmail.com>
2024-04-29 23:19:34 +0530

    first commit

---
+++ src/Endex.js
@@ -0,0 +1,43 @@
+
+import {FaStar} from 'react-icons/fa'
+
+import React, { useState } from 'react'
+import './style.css';
+
+
+    
+     function StarRating({noOfStar = 5}){
+
+        const [rating, setRating]= useState(0);
+        const [hover , setHover]= useState(0);
+
+        function handleClick(getCurrentIndex){
+         setRating(getCurrentIndex);
+        }
+
+        function handleMouseEnter(getCurrentIndex){
+        setHover(getCurrentIndex);
+        }
+        function handleMouseLeave(){
+          setHover(rating);
+
+        }
+        return <div className='star-rating'>
+
+            {
+                [...Array(noOfStar)].map((_,index)=> {
+                    index += 1
+                    return<FaStar
+                    key={index}
+                    className={index <= (hover || rating) ? 'active': 'inactive'}
+                    onClick={()=> handleClick(index)}
+                    onMouseMove={()=> handleMouseEnter(index)}
+                    onMouseLeave={()=> handleMouseLeave()}
+                    size={40}
+                    />
+                })
+            }
+        </div>
+    }
+
+    export default StarRating;
